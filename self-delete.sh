cd8e6e6032c55e12d00fce926b2ab62776076fcf
mskf3000 <32472679+mskf3000@users.noreply.github.com>
2020-04-22 07:42:43 -0300

    Update self-delete.sh

--- self-delete.sh
+++ self-delete.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
-sleep 3600s
+#!/bin/bash
+date;hostname -f
 export NAME=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/name -H 'Metadata-Flavor: Google')
 export ZONE=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/zone -H 'Metadata-Flavor: Google')
 gcloud --quiet compute instances delete $NAME --zone=$ZONE
+date;hostname -f
