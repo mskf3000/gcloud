6f10d93c3f925dde9036d0257343ca448618aaba
mskf3000 <32472679+mskf3000@users.noreply.github.com>
2020-04-22 07:45:17 -0300

    Create sd.sh

---
+++ sd.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+date;hostname -f
+export NAME=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/name -H 'Metadata-Flavor: Google')
+export ZONE=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/zone -H 'Metadata-Flavor: Google')
+gcloud --quiet compute instances delete $NAME --zone=$ZONE
+date;hostname -f
